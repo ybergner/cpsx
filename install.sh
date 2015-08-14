49772364065dde29e2196c380aa4b95409702f7b
ybergner <ybergner@gmail.com>
2015-08-13 22:22:22 -0400

    adding custom env files
    to help with installation

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
-vi /home/nunpa/IBL-Tutoring-Chat-XBlock/setup.py
-sudo -u edxapp /edx/bin/pip.edxapp install /home/nunpa/IBL-Tutoring-Chat-XBlock
-sudo /edx/bin/supervisorctl -c /edx/etc/supervisord.conf restart edxapp:
 
+# cd /edx/app/edxapp
+# sudo git clone https://github.com/ybergner/CPSX-xBlock/ --branch yoav --single-branch
+export CPSX_HOSTNAME="collaborative-assessment.org"
+
+sudo -u edxapp /edx/bin/pip.edxapp install xblock/ --upgrade
+sudo cp -r xblock/cpsx/public /edx/app/edxapp/venvs/edxapp/local/lib/python2.7/site-packages/cpsx/
+sudo /edx/bin/supervisorctl -c /edx/etc/supervisord.conf restart edxapp:
