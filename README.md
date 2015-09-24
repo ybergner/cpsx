c39f55edae7c9ca4f8de3aeef5d6519d666766cd
Yoav Bergner <ybergner@gmail.com>
2015-09-24 17:44:54 -0400

    update documentation

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Collaborative Problem Solving Xblock (cpsx)
+# Collaborative Problem Solving XBlock (cpsx)
 
 The purpose of this XBlock is to allow small groups to work on a set of edX problems together through real-time chat.
 The problems in the assignment can be any type of edX problem, e.g. numerical input, drag-and-drop, etc. 
@@ -10,15 +10,18 @@ database on the MySQL server, installing the xblock, and manually copying some s
 
 ## Basic Functionality
 
-* The instructor creates in Studio an (advanced type) Collaborative Problem Solving Unit assignment and decides the following:
-  * the room name, which allows a single chat session to follow students across different screens within an assignment
-  * the size of a collaboration cohort (group size), e.g. 2 or 3 students
-  * the wait time, e.g. 5 minutes, to synchronize students before starting the assignment 
+* To create a collaborative assignment in Studio, make sure "cpsx" is added to advanced modules list
+* Insert an (advanced type) Collaborative Problem Solving element in any unit, and edit the following:
+  * room name, which allows a single chat session to follow students across different screens within an assignment
+  * group size of a collaboration cohort, e.g. 2 or 3 students
+  * wait time, e.g. 5 minutes, to synchronize students before starting the assignment 
 and to put a limit on the time a student will wait in the event that no partners are available
-* A copy of the CPS unit must be placed on every page where the chat should appear.
-* Students who may lose their connection will be able to rejoin their last conversation; for this reason, students must explicitly 
-log out of their chat session to remove themselves from the active membership list.
-* Chat logs are stored in a database called ajax_chat via the OpenEdX MySQL server
+* A copy of the CPS unit must be placed on every page where the chat should appear. 
+The CPS element will check for an active chat session of the current user and load the chat transcript.
+* Students who might lose their network connection will be able to rejoin their last conversation; for this reason, they must explicitly 
+log out of their chat session to remove themselves from the active membership list. 
+(It is straightforward to create garbage-cleaning scripts to close out active chat sessions.)
+* Chat logs and chatroom membership assignments are stored in a MySQL database called ajax_chat.
 
 
 
--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 ##
 ## INSTALLATION INSTRUCTIONS 
+##
+## WARNING: although this file is set up as a bash script,
+## it is not intended to be used as a single-purpose installer.
+## However, bits and pieces may reasonably be used!
+##
 
 ## We will try to do all of this starting from a barebones Ubuntu installation using
-## the birch.2 release (see here for updated information
+## the cypress release (see here for updated information
 ## https://github.com/edx/configuration/wiki/edX-Ubuntu-12.04-64-bit-Installation)
 
 ## On Amazon, if using a community Ubuntu 12.04 64 bit AMI, it is necessary to expand the
@@ -13,32 +18,36 @@
 
 ## AMI ID ubuntu-precise-12.04-amd64-server-20150401 (ami-00615068)
 
+## Bare cypress installation as follows:
+
 ## Connect to the remote server
 
 ## sudo apt-get update -y
 ## sudo apt-get upgrade -y
 ## sudo reboot
 
-export OPENEDX_RELEASE=named-release/cypress
-wget https://raw.githubusercontent.com/edx/configuration/master/util/install/sandbox.sh -O - | bash
+## export OPENEDX_RELEASE=named-release/cypress
+## wget https://raw.githubusercontent.com/edx/configuration/master/util/install/sandbox.sh -O - | bash
 
 
 # To set up the virtual host without interfering with other OpenEdX ports, 
-# we will use port 4444 in the attached files
+# we will use apache2 on port 4444 in the subsequent steps
+# On OpenEdX, apache installation will fail because port 80 is already in use by nginx.
+# Later we will replace the appropriate files
 
 sudo apt-get install apache2
 sudo apt-get install php5 php5-mysql libapache2-mod-php5
 sudo apt-get install php5 libapache2-mod-php5 libapache2-mod-auth-mysql php5-mysql
 
-# On OpenEdX, apache installation will fail because port 80 is already in use by nginx.
-# Later we will replace the appropriate files
 
 # Modify /etc/apache2/ports.conf (replace with included version)
 # Add the file chatapp.conf to /etc/apache2/sites-available
 # and then symlink it to sites/enabled
 
 cd /edx/app/edxapp
-sudo -u edxapp git clone https://github.com/ybergner/cpsx/ --branch yoav
+sudo -u edxapp git clone https://github.com/ybergner/cpsx
+
+### IMPORTANT: You must edit chatapp.conf to reflect your server's domain name
 sudo cp -r cpsx/chatapp /var/www
 sudo cp -r cpsx/ports.conf /etc/apache2/
 sudo cp -r cpsx/chatapp.conf /etc/apache2/sites-available/
@@ -57,10 +66,7 @@ sudo /etc/init.d/apache2 start
 # The app still won't work until the databases it needs are created.
 # Create a MySQL DDBB and import the structure from the sql.dump
 
-mysql -u root create database ajax_chat
-
-# Dump MySQL structure to the new created DDBB,
-
+mysql -u root -e "create database ajax_chat"
 mysql -u root ajax_chat < cpsx/chatapp/mysql-dump/sql.dump
 
 # can test chatapp by visiting collaborative-assessment.org:4444
@@ -68,16 +74,15 @@ mysql -u root ajax_chat < cpsx/chatapp/mysql-dump/sql.dump
 
 # Need to add this line to cms.envs.json under FEATURES
 # "ALLOW_ALL_ADVANCED_COMPONENTS": true,
-# or just replace cms envs file with this one
-sudo cp cpsx/cypress_envs/cms.env.json .
-sudo chown edxapp:edxapp cms.env.json
+# or just replace default cms envs file with this one
+sudo -u edxapp cp cpsx/cypress_envs/cms.env.json .
 # OPTIONAL: sudo cp cpsx/cypress_envs/lms.env.json .
 
 # Now install the XBlock
 sudo -u edxapp /edx/bin/pip.edxapp install cpsx/xblock/
+# and copy over these resources
 sudo cp -r cpsx/xblock/cpsx/public /edx/app/edxapp/venvs/edxapp/local/lib/python2.7/site-packages/cpsx/
 # # If installed: Upgrade the XBlock using --upgrade
-# sudo -u edxapp /edx/bin/pip.edxapp install yourXBlock/ --upgrade
 
 # restart the cms
 sudo /edx/bin/supervisorctl restart edxapp:
